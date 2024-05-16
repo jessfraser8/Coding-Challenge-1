dbf9d226838930001fb658b7ed53142225d0f7d7
jessfraser8 <jfraser4@usf.edu>
2024-05-16 19:46:24 -0400

    Create and test calcTip function.

--- cc_1_script.js
+++ cc_1_script.js
@@ -1,2 +1,8 @@
+let bill = 100
 let tip = bill <= 300 && bill >= 50 ? '.15' : '.2';
-console.log("The bill was $" + bill + ", the tip was $" + tip + ", and the total was $" + total + ".");
+function calcTip(bill){
+    return bill * tip
+};
+calcTip(100)
+let total = bill + calcTip(100)
+console.log("The bill was $" + bill + ", the tip was $" + calcTip(100) + ", and the total was $" + total + ".");
